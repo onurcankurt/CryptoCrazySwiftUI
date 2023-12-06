e7faa71794dbb672a2a16696fb22453cfeda41a0
Onurcan Kurt <onurcankurt96@gmail.com>
2023-12-06 17:40:47 +0300

    MVVM files created, Crypto model created

---
+++ CryptoCrazySUI/Model/CryptoCurrency.swift
@@ -0,0 +1,21 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazySUI
+//
+//  Created by onur on 6.12.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency : Hashable, Decodable, Identifiable {
+    
+    let id = UUID()
+    let currency : String
+    let price : String
+    
+    private enum CodingKeys : String, CodingKey {
+        case currency = "currency"
+        case price = "price"
+    }
+    
+}
---
+++ CryptoCrazySUI/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CryptoCrazySUI
+//
+//  Created by onur on 6.12.2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+#Preview {
+    MainView()
+}
